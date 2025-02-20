380ef30ee9ccb58476eb111fc182a8aaea967347
Cecilia Rava <ravacecilia1@gmail.com>
2025-02-20 08:08:30 -0300

    feat: Learn datatypes and do exercises

---
+++ 00-exercises.js
@@ -0,0 +1,70 @@
+/*
+Class 18 - Exercises: First Steps
+Video: https://youtu.be/1glVfFxj8a4?t=4733
+*/
+
+// 1. Write a single-line comment
+// Hello World!
+
+// 2. Write a multi-line comment
+/* 
+This is a multi-line
+comment
+*/
+
+// 3. Declare variables with values associated with all primitive data types
+let string = 'Hello'
+let integer = 6
+let float = 9.7
+let boolean = false
+let undefined
+let nullValue = null
+let symbol = Symbol('mySymbol')
+let bigInt = BigInt(244783947328947239749)
+
+// 4. Print the value of all variables to the console
+console.log(' ******** Variable values ********')
+console.log(string)
+console.log(integer)
+console.log(float)
+console.log(boolean)
+console.log(undefined)
+console.log(nullValue)
+console.log(symbol)
+console.log(bigInt)
+
+// 5. Print the type of all variables to the console
+console.log(' ******** Variable types ********')
+console.log(typeof string)
+console.log(typeof integer)
+console.log(typeof float)
+console.log(typeof boolean)
+console.log(typeof undefined)
+console.log(typeof nullValue)
+console.log(typeof symbol)
+console.log(typeof bigInt)
+
+// 6. Next, modify the values of the variables with others of the same type
+string = 'Bye'
+integer = 3
+float = 4.7
+boolean = true
+undefined
+nullValue = null
+symbol = Symbol('Symbol')
+bigInt = BigInt(24478323947328947239749)
+
+// 7. Next, modify the values of the variables with others of a different type
+// I can't modify the type of a variable
+
+// 8. Declare constants with values associated with all primitive data types
+const STRING = 'Hello'
+const INTEGER = 6
+const FLOAT = 9.7
+const BOOLEAN = false
+const NULL_VALUE = null
+const SYMBOL = Symbol('mySymbol')
+const BIGINT = BigInt(244783947328947239749)
+
+// 9. Next, modify the values of the constants
+// I can't modify a constant
--- 00-helloworld.js
+++ 00-helloworld.js
@@ -1,1 +1,25 @@
 console.log('Hello world')
+// Variables
+// var: Don't use
+// let: Variables have effect inside a context (code section)
+// const
+
+// Datatypes: Primitive
+let string = 'hello'
+let integer = 1
+let float = 1.2
+let boolean = true
+let undefined // Value undefined
+let nullValue = null // Value undefined intentionally
+let symbol = Symbol('symbol') // Unique values to identify properties of objects
+let bigInt = BigInt('5345637895758947589357894375897589843975')
+
+// Show datatypes
+console.log(typeof string)
+console.log(typeof integer)
+console.log(typeof float)
+console.log(typeof boolean)
+console.log(typeof undefined)
+console.log(typeof nullValue)
+console.log(typeof symbol)
+console.log(typeof bigInt)
