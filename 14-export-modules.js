448ea7f805e20b40698b153c66dcbd53b410aec5
Cecilia Rava <ravacecilia1@gmail.com>
2025-02-25 11:21:54 -0300

    feat: Learn about import and export modules

---
+++ 14-export-modules.js
@@ -0,0 +1,8 @@
+// Way 1: Change export and import to mjs
+// Way 2: Create a package.json
+
+export function add(a, b) {
+    return a + b
+}
+
+// console.log(add(3, 2))
---
+++ 14-import-modules.js
@@ -0,0 +1,6 @@
+// Way 1: Change import and export to mjs
+// Way 2: Create a package.json
+
+import { add } from "./14-export-modules.js"
+
+console.log(add(2, 2))
